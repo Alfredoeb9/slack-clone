8d694f8966e3efdd490ee3f87166a0dc2337f5ce
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-13 19:35:03 -0700

    updated code

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
 import SidebarOption from './SidebarOption';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import db from './firebase';
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    // Run this code ONCE when the sidebar component loads using []
+    // take new snapshot of the database and return it, gets fired everytime something changes in the database
+    db.collection('rooms').onSnapshot(snapshot => (
+      setChannels(
+        snapshot.docs.map(doc => ({
+          id: doc.id,
+          name: doc.data().name
+        }))
+        )
+    ));
+  }, []);
+
+
   return (
     <div className="sidebar">
       <div className="sidebar_header">
@@ -17,10 +43,25 @@ function Sidebar() {
           </h3>
         </div>
         <CreateIcon />
-
       </div>
       <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-      <SidebarOption title="Youtube" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channels" />
+
+      {/* Conect to DB and list all the channels */}
+      {/* <SidebarOption ... /> */}
+      {channels.map(channel => (
+        <SidebarOption title={channel.name} id={channel.id} />
+      ))}
     </div>
   );
 }
