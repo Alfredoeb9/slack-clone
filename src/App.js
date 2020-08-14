7c53b4a221a9916bbec73abe9667501c5b50b633
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-13 19:31:52 -0700

    Added router to pull channel from database and switch cases for each channel

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the Slack Clone</h1>
-      {/* Header */}
+    <Router>
       <Header />
       <div className="app_body">
         <Sidebar />
-        {/* React-Router -> Chat screen */}
+
+        <Switch>
+          <Route path="/room/:roomId">
+            <Chat />
+          </Route>
+          <Route path="/">
+            <h1>Welcome</h1>
+          </Route>
+        </Switch>
       </div>
+    </Router>
+      
     </div>
   );
 }
