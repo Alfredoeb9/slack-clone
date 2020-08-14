0fd65b913d3226e36ea2f3b634c58245e32c2085
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-13 19:33:38 -0700

    Added func to sidebar connected add channel button to database and useHistory hook to track history

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import "./SidebarOption.css";
+import { useHistory } from 'react-router-dom';
+import db from './firebase';
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
+  // if we click backarrow or forward arrow save the history
+  const history = useHistory();
+
+  // when you click on the rooms push the room(ID) into the url
+  const selectChannel = () => {
+    if(id) {
+      history.push(`/room/${id}`);
+    } else {
+      history.push('title');
+    }
+  };
+
+  const addChannel = () => {
+    const channelName = prompt('Please enter the channel name');
+
+    // add channel names to database in firebase
+    if(channelName) {
+      db.collection('rooms').add({
+        name: channelName
+      });
+    }
+  };
+
   return (
-    <div className="sidebarOption">
+    // if the add channel option is clicked return select menu
+    <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
       {/* Only render an Icon if it exists */}
       { Icon && <Icon className="sidebarOption_icon" />}
       {/* If no channel has an icon put hashtag */}
       { Icon ? (
         <h3>{title}</h3>
       ) : (
-        <h3>
+        <h3 className="sidebarOption_channel">
           <span className="sidebarOption_hash">#</span> {title}
         </h3>
       )}
